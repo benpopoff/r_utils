84e70667b005f8eda73479410b10bcef81f5b04f
Benjamin Popoff <popoff@protonmail.com>
2020-03-11 12:20:00 +0100

    Add extract_number

--- utils.R
+++ utils.R
@@ -1,11 +1,20 @@
 # Missing values report in a dataframe
 summarise_na <- function(df){
-	result <- df %>%
-		dplyr::summarise_all(list(~sum(is.na(.)))) %>%
-		tidyr::pivot_longer(everything(),
-			names_to = "variable",
-			values_to = "nb_na") %>%
-		dplyr::arrange(desc(nb_na))
+  result <- df %>%
+    dplyr::summarise_all(list(~sum(is.na(.)))) %>%
+    tidyr::pivot_longer(everything(),
+                 names_to = "variable",
+                 values_to = "nb_na") %>%
+    dplyr::arrange(desc(nb_na))
+  
+  result
+}
 
-	result
+# Extract numeric values from raw variable
+extract_number <- function(x){
+  result <- stringr::str_extract(x, "([0-9]*,?[0-9]+)") %>%
+    stringr::str_replace(",", ".") %>%
+    stringr::as.numeric()
+  
+  result
 }
