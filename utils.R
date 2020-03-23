1bc0530c29517017c3e72f42653f621dbd47ae6c
Benjamin Popoff <popoff@protonmail.com>
2020-03-23 15:45:20 +0100

    Add view_na

--- utils.R
+++ utils.R
@@ -10,6 +10,19 @@ summarise_na <- function(df){
   result
 }
 
+# View missing values in a plot
+view_na <- function(df){
+  df %>% 
+    summarise_na() %>%
+    ggplot2::ggplot(
+      ggplot2::aes(x = reorder(variable, nb_na), y = nb_na)) +
+    ggplot2::geom_bar(stat = "identity", fill = "#455a64") +
+    ggplot2::geom_text(aes(label = nb_na), hjust = -0.2) +
+    ggplot2::coord_flip() +
+    ggplot2::labs(y = "Missing values",
+         x = "Variables")
+}
+
 # Extract numeric values from raw value
 extract_number <- function(x){
   result <- stringr::str_extract(x, "([0-9]*,?[0-9]+)") %>%
